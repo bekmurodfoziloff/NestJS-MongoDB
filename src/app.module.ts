d1bb0b1143a27acfdfbf06db4a17e12e96036ecb
BekmurodFoziloff <bekmurodfoziloff@gmail.com>
2023-02-02 12:11:35 +0500

    feta(Database): create MongoDB configuration and connected to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_HOST: Joi.string().required(),
+        MONGO_PORT: Joi.number().required(),
+        MONGO_DATABASE: Joi.string().required(),
+        PORT: Joi.number()
+      })
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const host = configService.get('MONGO_HOST');
+        const port = configService.get('MONGO_PORT');
+        const database = configService.get('MONGO_DATABASE');
+        return {
+          uri: `mongodb://${host}:${port}`,
+          dbName: database,
+          useNewUrlParser: true
+        };
+      },
+      inject: [ConfigService]
+    })
+  ],
+  controllers: [],
+  providers: []
 })
 export class AppModule {}
